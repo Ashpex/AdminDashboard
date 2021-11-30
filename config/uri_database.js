92a39cec24bcb028f304314309b8d80ea07e87e4
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2021-11-30 21:31:22 +0700

    vuong updates url db

--- config/uri_database.js
+++ config/uri_database.js
@@ -1,2 +1,2 @@
 module.exports.URI_DATABASE =
-  "mongodb+srv://vuong:vuong19022001@cluster0.o6he2.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://vuong:vuong19022001@cluster0.o6he2.mongodb.net/organi_shop?retryWrites=true&w=majority";
