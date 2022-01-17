c55894786cc32075722ca3e783421b8150dd66cc
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2022-01-17 15:55:58 +0700

    add notification

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const exphbs = require("express-handlebars");
 const path = require("path");
 const bodyParser = require("body-parser");
 const Handlebars = require("handlebars");
+const socket = require("socket.io");
+const cors = require("cors");
 const {
     allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
@@ -18,6 +20,7 @@ const ProducerRoute = require("./routes/producer.route");
 const RevenueRouter = require("./routes/revenue.route");
 const OrdersRoute = require("./routes/orders.route");
 const ApiChartRoute = require("./api/chart/apiChart.route");
+const ApiNotificationRoute = require("./api/notification/apiNotification.route");
 const SessionMiddleware = require("./middlewares/session");
 const PassportMiddleware = require("./middlewares/passport");
 const LocalsMiddleware = require("./middlewares/locals");
@@ -61,7 +64,7 @@ app.use(LocalsMiddleware);
 //   }
 // });
 
-app.get("/", function (req, res) {
+app.get("/", AuthMiddleware, function (req, res) {
     res.render("home");
 });
 
@@ -74,6 +77,7 @@ app.use("/producer", AuthMiddleware, ProducerRoute);
 app.use("/orders", AuthMiddleware, OrdersRoute);
 app.use("/revenue", AuthMiddleware, RevenueRouter);
 app.use("/api/chart", AuthMiddleware, ApiChartRoute);
+app.use("/api/notification", AuthMiddleware, ApiNotificationRoute);
 
 app.use((req, res) => {
     res.render("errors/404", { layout: false });
@@ -84,6 +88,20 @@ app.use((err, req, res, next) => {
     res.status(500).render("errors/500", { layout: false, error: err.message });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("App listening on port 3000");
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`App listening on port ${process.env.PORT || 3000}`);
+});
+
+const io = socket(server);
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
+
+    socket.on("disconnect", function () {
+        console.log("Made socket disconnected");
+    });
+
+    socket.on("send-notification", function (data) {
+        // io.emit("new-notification", data);
+        socket.broadcast.emit("new-notification", data);
+    });
 });
